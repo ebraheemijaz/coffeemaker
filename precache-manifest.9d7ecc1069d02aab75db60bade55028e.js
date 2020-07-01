5f43d9b5251556e705ecac6a57c452a29a6255b0
Ebraheem <ebraheemijaz913@gmail.com>
2020-07-01 19:24:59 +0500

    Updates

---
+++ precache-manifest.9d7ecc1069d02aab75db60bade55028e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c809d57ef850b650f3e5fd9b99776fc",
+    "url": "/coffeemaker/index.html"
+  },
+  {
+    "revision": "cd74df6203bc690f97a6",
+    "url": "/coffeemaker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cd081593fa998988fe1a",
+    "url": "/coffeemaker/static/js/2.735f21a8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/coffeemaker/static/js/2.735f21a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd74df6203bc690f97a6",
+    "url": "/coffeemaker/static/js/main.f8b5fd64.chunk.js"
+  },
+  {
+    "revision": "d3546cdb0b8846dd3ed4",
+    "url": "/coffeemaker/static/js/runtime-main.3c126ab7.js"
+  }
+]);
